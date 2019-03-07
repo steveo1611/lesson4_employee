6b036d5aaf83347791c7a8b117dee12376f48124
Steve Oldfield <steve.oldfield@intimetec.com>
2019-03-07 10:59:10 -0700

    Add project files.

---
+++ Lesson4-employee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson4_employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
